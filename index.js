3adb41102183fbfa017e33c14b4be80e60db1600
a.breezy <ambrose.wilkins@gmail.com>
2021-12-04 11:22:29 -0500

    fill generateMarkdown.js

--- index.js
+++ index.js
@@ -1,64 +1,86 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generateMarkdown = require('./util/generateMarkdown');
-const fs = require('fs');
-const path = requite('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-  {
-    type: 'input', 
-    name: 'title',
-    message: 'Please input Project Title'
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Please give a description for your project'
-  },
-  {
-    type: 'input',
-    name: 'Installation Instructions',
-    message: 'Please give instructions for how to intall'
-  },
-  {
-    type: 'input',
-    name: 'Usage',
-    message: 'Please give a description of how to use'
-  },
-  {
-    type: 'list',
-    name: 'License',
-    message: 'Choose a license type',
-    choices: [" // search license types and include"]
-  },
-  {
-    type: 'input',
-    name: 'Contributing',
-    message: '//Search what contributing means...'
-  },
-  {
-    type: 'input',
-    name: 'Test',
-    message: 'Instructions on how to test App'
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'If you have any questions email me'
-  }];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-
-}
+	{
+		type: "input",
+		name: "title",
+		message: "Please input Project Title",
+		validate: (titleInput) => {
+			if (titleInput) {
+				return true;
+			} else {
+				console.log("Please enter a title for your project");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "description",
+		message: "Please give a description for your project",
+		validate: (descriptionInput) => {
+			if (descriptionInput) {
+				return true;
+			} else {
+				console.log("Please enter a description for your project");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "installationInstructions",
+		message: "Please give instructions for how to intall",
+		validate: (installationInstructionsInput) => {
+			if (installationInstructionsInput) {
+				return true;
+			} else {
+				console.log(
+					"Please enter a installation instructions for your project"
+				);
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: "Please give a description of how to use",
+	},
+	{
+		type: "list",
+		name: "license",
+		message: "Choose a license type",
+		choices: ["Apache", "GPL", "IBM", "MOZILLA", "MIT", "None"],
+	},
+	{
+		type: "input",
+		name: "contributing",
+		message: "List collaborators",
+	},
+	{
+		type: "input",
+		name: "test",
+		message: "Instructions on how to test App",
+	},
+	{
+		type: "input",
+		name: "email",
+		message: "If you have any questions email me",
+	},
+];
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((userResponses) => {
-    writeToFile('readme.md', generateMarkdown({ ...userResponses}));
-  });
+	inquirer.prompt(questions).then((userResponses) => {
+		fs.writeFile("./dist/readme.md", userResponses, (err) => {
+			err ? console.log(err) : console.log("generated File");
+		});
+	});
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+	switch (license) {
+		case "APACHE 2.0":
+			return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+		case "GPL 3.0":
+			return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+		case "IBM":
+			return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+		case "MIT":
+			return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+		case "MOZILLA":
+			return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    case "None":
+      return "";
+	};
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "APACHE 2.0":
+			return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+		case "GPL 3.0":
+			return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+		case "IBM":
+			return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+		case "MIT":
+			return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+		case "MOZILLA":
+			return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    case "None":
+      return "";
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,8 +42,45 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-// add text to input for the ReadMe.md
+	return `# ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Badges](#badges)
+  * [Features](#features)
+  * [How to Contribute](#contribute)
+  * [Tests](#tests)
+
+  ## Installation
+  ${data.instillation}
+
+  ## Usage
+  ${usage}
+
+  ## Credits
+  ${credits}
+
+  ## License
+  ${license}
+
+  ## Badges
+  ${badges}
+
+  ## Features 
+  ${features}
+
+  ## How to Contribute
+  ${contribute}
+
+  ## Tests 
+  ${tests}
 `;
 }
 
